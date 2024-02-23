94bfea56b7ac83c8c852aa71c953505224974088
Ethan Ruszanowski <ethan@rusz.dev>
2024-02-22 23:26:03 -0500

    Update database creation script

--- database-scripts/10_create.sql
+++ database-scripts/10_create.sql
@@ -4,13 +4,42 @@ create table if not exists moon_tier
     tier_name    text not null
 );
 
+create table if not exists risk_level
+(
+    risk_level_id   int primary key,
+    risk_level_name text not null
+);
+
+create table if not exists layout
+(
+    layout_id   int primary key,
+    layout_name text not null
+);
+
+create table if not exists weather
+(
+    weather_id   int primary key,
+    weather_name text not null,
+    effect       text
+);
+
 create table if not exists moon
 (
-    moon_id           int primary key,
-    moon_name         text not null,
-    moon_tier_id      int  not null,
-    outside_max_power int  not null,
-    inside_max_power  int  not null,
+    moon_id             int primary key,
+    moon_name           text not null,
+    risk_level_id       int  not null,
+    cost                int  not null,
+    default_layout_id   int  not null,
+    map_size_multiplier real not null,
+    min_scrap           int  not null,
+    max_scrap           int  not null,
+    outside_max_power   int  not null,
+    inside_max_power    int  not null,
+    moon_tier_id        int  not null,
+    foreign key (risk_level_id)
+        references risk_level (risk_level_id),
+    foreign key (default_layout_id)
+        references layout (layout_id),
     foreign key (moon_tier_id)
         references moon_tier (moon_tier_id)
 );
@@ -39,6 +68,27 @@ create table if not exists creature
         references creature_type (creature_type_id),
     foreign key (favorite_moon_id)
         references moon (moon_id)
+);
+
+create table if not exists scrap
+(
+    scrap_id   int primary key,
+    scrap_name text not null,
+    min_value  int  not null,
+    max_value  int  not null,
+    weight     int  not null,
+    conductive int  not null,
+    two_handed int  not null
+);
+
+create table if not exists viable_weather
+(
+    moon_id    int not null,
+    weather_id int not null,
+    foreign key (moon_id)
+        references moon (moon_id),
+    foreign key (weather_id)
+        references weather (weather_id)
 );
 
 create table if not exists spawn_chance
